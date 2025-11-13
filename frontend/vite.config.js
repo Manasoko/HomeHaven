648a102786b9ea81ce0ba0bdf0d0e3bc9d404812
Manasoko <manajiyah@gmail.com>
2025-11-14 00:08:46 +0100

    feat: Add TypeScript configuration files for app and node environments; integrate Tailwind CSS in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   define: {
     'process.env.REACT_API_URI' : JSON.stringify(process.env.REACT_API_URI)
   }
